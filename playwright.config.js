4d9e12ca2ca03777fa188789d5cf2437d8f9dab9
Paresh-Mandlecha <mandlechaparesh@gmail.com>
2026-02-02 12:45:06 +0530

    Add playwright config file

--- playwright.config.js
+++ playwright.config.js
@@ -2,23 +2,37 @@
 const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
-  testDir: './tests',
-
-  timeout: 30 * 1000,
-
+testDir: './tests',
+  /*Run Test files in Parallel*/
+  fullyParallel:true,
+  /*Fail the build on CI if you accidently left test.only in the source code.*/
+  forbidOnly:!!process.env.CI,
+  /*Retry on CI only*/
+  //retries:process.env.CI ? 2 : 0,
+  // retries:1,
+  /*Opt out of parallel tests on CI.*/
+  workers:1,
+  //process.env.CI ? 1 : undefined,
+  /*reporter to use.see https://playwright.dev/docs/test-reporters*/
+  // reporter: 'html',
+   //reporter:[['json',{outputFile:'results.json'}]]
+   //reporter:[['junit',{outputFile:'results.xml'}]]
+   reporter:[['list'],
+             ['html'],
+             ['junit',{outputFile:'results.xml'}],
+             ['json',{outputFile:'results.json'}]
+  ],
   expect: {
     timeout: 5000
   },
 
-  reporter: 'html',
-
   use: {
-    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
   },
 
+  /*configure projects for major browsers*/
   projects: [
     {
       name: 'chromium',
